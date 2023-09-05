6ed1d78b9842e364690dca400284f0b7e89cfab5
Brady Wallace <bw030203@gmail.com>
2023-09-05 10:04:05 -0600

    Added random spawing

---
+++ Brady's Brick Game/Assets/Scripts/SpawnObjectsBehaviour.cs
@@ -0,0 +1,61 @@
+
+using UnityEngine;
+
+public class SpawnObjectsBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject[] bricks;
+
+    [SerializeField] private GameObject[] paddlePowerUps;
+
+    [SerializeField] private GameObject[] ballPowerUps;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < bricks.Length; i++)
+        {
+            float ran = Random.Range(0.0f, 1.0f);
+            if (ran <= 0.8f)
+            {
+                ran = Random.Range(0.0f, 1.0f);
+                if (ran <= 0.90f)
+                {
+                    bricks[i].SetActive(true);
+                }
+
+                else
+                {
+                    ran = Random.Range(0.0f, 1.0f);
+                    if (ran <= 0.51f)
+                    {
+                        for (int j = 0; j < paddlePowerUps.Length; j++)
+                        {
+                            if (!paddlePowerUps[j].activeSelf)
+                            {
+                                paddlePowerUps[j].SetActive(true);
+                                paddlePowerUps[j].transform.position = bricks[i].transform.position;
+                                break;
+                            }
+
+
+                        }
+                    }
+                    else
+                    {
+                        for (int k = 0; k < ballPowerUps.Length; k++)
+                        {
+                            if (!paddlePowerUps[k].activeSelf)
+                            {
+                                paddlePowerUps[k].SetActive(true);
+                                paddlePowerUps[k].transform.position = bricks[i].transform.position;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    }
+
